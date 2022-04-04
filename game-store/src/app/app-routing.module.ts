3572f9786cab0e1aeffca25cae9dccf3426e22cc
Kate <kate.schlagel@relationshipone.com>
2022-04-04 14:32:08 -0500

    set up routing between search and checkout. styling and layout changes

--- game-store/src/app/app-routing.module.ts
+++ game-store/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SearchComponent } from './components/store/search/search.component';
+import { CheckoutComponent } from './components/store/checkout/checkout.component';
 import { StoreComponent } from './components/store/store.component';
 
 const routes: Routes = [
+  { path: '', component: StoreComponent },
   { path: 'index.html', component: StoreComponent },
-  { path: '', component: StoreComponent }
+  { path: 'store', component: StoreComponent },
+  { path: 'checkout', component: CheckoutComponent}
 ];
 
 @NgModule({
